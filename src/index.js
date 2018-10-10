fc89ce2337c7d2a49272e08673a76a81957c4341
Bitaru <i@bitaru.com>
2018-10-10 17:16:31 +0300

    fix reducers path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import * as selectors from './selectors';
 import * as actions from './actions';
-export { createReducer, shelf } from './reducers';
+export { createReducer, shelf } from './reducer';
 export { selectors, actions };
